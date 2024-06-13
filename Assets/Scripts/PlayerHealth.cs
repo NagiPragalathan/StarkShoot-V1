d536243015a8c7089717ff9a49f65489e5e3fa4b
NagiPragalathan <nagipragalathan@gmail.com>
2024-06-13 18:07:04 +0530

    "Updated PhotonServerSettings, PlayerHealth script, and Unity layout settings."

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,8 +7,8 @@ using System.Collections;
 [RequireComponent(typeof(FirstPersonController))]
 [RequireComponent(typeof(Rigidbody))]
 
-public class PlayerHealth : MonoBehaviourPunCallbacks, IPunObservable {
-
+public class PlayerHealth : MonoBehaviourPunCallbacks, IPunObservable
+{
     public delegate void Respawn(float time);
     public delegate void AddMessage(string Message);
     public event Respawn RespawnEvent;
@@ -45,54 +45,56 @@ public class PlayerHealth : MonoBehaviourPunCallbacks, IPunObservable {
     private bool isDead;
     private bool isSinking;
     private bool damaged;
+    private NetworkManager networkManager;
 
-    /// <summary>
-    /// Start is called on the frame when a script is enabled just before
-    /// any of the Update methods is called the first time.
-    /// </summary>
-    void Start() {
+    void Start()
+    {
         fpController = GetComponent<FirstPersonController>();
         ikControl = GetComponentInChildren<IKControl>();
         damageImage = GameObject.FindGameObjectWithTag("Screen").transform.Find("DamageImage").GetComponent<Image>();
         healthSlider = GameObject.FindGameObjectWithTag("Screen").GetComponentInChildren<Slider>();
         currentHealth = startingHealth;
-        if (photonView.IsMine) {
+        if (photonView.IsMine)
+        {
             gameObject.layer = LayerMask.NameToLayer("FPSPlayer");
             healthSlider.value = currentHealth;
         }
         damaged = false;
         isDead = false;
         isSinking = false;
+        networkManager = FindObjectOfType<NetworkManager>();
     }
 
-    /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
-    void Update() {
-        if (damaged) {
+    void Update()
+    {
+        if (damaged)
+        {
             damaged = false;
             damageImage.color = flashColour;
-        } else {
+        }
+        else
+        {
             damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
         }
-        if (isSinking) {
+        if (isSinking)
+        {
             transform.Translate(Vector3.down * sinkSpeed * Time.deltaTime);
         }
     }
 
-    /// <summary>
-    /// RPC function to let the player take damage.
-    /// </summary>
-    /// <param name="amount">Amount of damage dealt.</param>
-    /// <param name="enemyName">Enemy's name who cause this player's death.</param>
     [PunRPC]
-    public void TakeDamage(int amount, string enemyName) {
+    public void TakeDamage(int amount, string enemyName)
+    {
         if (isDead) return;
-        if (photonView.IsMine) {
+        if (photonView.IsMine)
+        {
             damaged = true;
             currentHealth -= amount;
-            if (currentHealth <= 0) {
+            if (currentHealth <= 0)
+            {
                 photonView.RPC("Death", RpcTarget.All, enemyName);
+                networkManager.HandleDeath(PhotonNetwork.LocalPlayer.NickName);
+                networkManager.HandleKill(enemyName);
             }
             healthSlider.value = currentHealth;
             animator.SetTrigger("IsHurt");
@@ -101,41 +103,33 @@ public class PlayerHealth : MonoBehaviourPunCallbacks, IPunObservable {
         playerAudio.Play();
     }
 
-    /// <summary>
-    /// RPC function to declare death of player.
-    /// </summary>
-    /// <param name="enemyName">Enemy's name who cause this player's death.</param>
     [PunRPC]
-    void Death(string enemyName) {
+    void Death(string enemyName)
+    {
         isDead = true;
         ikControl.enabled = false;
         nameTag.gameObject.SetActive(false);
-        if (photonView.IsMine) {
+        if (photonView.IsMine)
+        {
             fpController.enabled = false;
             animator.SetTrigger("IsDead");
             AddMessageEvent(PhotonNetwork.LocalPlayer.NickName + " was killed by " + enemyName + "!");
             RespawnEvent(respawnTime);
-            StartCoroutine("DestoryPlayer", respawnTime);
+            StartCoroutine("DestroyPlayer", respawnTime);
         }
         playerAudio.clip = deathClip;
         playerAudio.Play();
         StartCoroutine("StartSinking", sinkTime);
     }
 
-    /// <summary>
-    /// Coroutine function to destory player game object.
-    /// </summary>
-    /// <param name="delayTime">Delay time before destory.</param>
-    IEnumerator DestoryPlayer(float delayTime) {
+    IEnumerator DestroyPlayer(float delayTime)
+    {
         yield return new WaitForSeconds(delayTime);
         PhotonNetwork.Destroy(gameObject);
     }
 
-    /// <summary>
-    /// RPC function to start sinking the player game object.
-    /// </summary>
-    /// <param name="delayTime">Delay time before start sinking.</param>
-    IEnumerator StartSinking(float delayTime) {
+    IEnumerator StartSinking(float delayTime)
+    {
         yield return new WaitForSeconds(delayTime);
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.useGravity = false;
@@ -143,17 +137,15 @@ public class PlayerHealth : MonoBehaviourPunCallbacks, IPunObservable {
         isSinking = true;
     }
 
-    /// <summary>
-    /// Used to customize synchronization of variables in a script watched by a photon network view.
-    /// </summary>
-    /// <param name="stream">The network bit stream.</param>
-    /// <param name="info">The network message information.</param>
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
-        if (stream.IsWriting) {
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
             stream.SendNext(currentHealth);
-        } else {
+        }
+        else
+        {
             currentHealth = (int)stream.ReceiveNext();
         }
     }
-
 }
