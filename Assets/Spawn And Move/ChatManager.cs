d44dddaa814ba7501fce6c76dd19f489511844f6
NagiPragalathan <nagipragalathan@gmail.com>
2024-06-23 21:53:56 +0530

    final commit

---
+++ Assets/Spawn And Move/ChatManager.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using Dojo;
+using Dojo.Starknet;
+using Dojo.Torii;
+using dojo_bindings;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatManager : MonoBehaviour
+{
+    public bool chatOpen = false;
+    public FieldElement channel = new FieldElement(0);
+
+    public GameManager gameManager;
+    public WorldManager worldManager;
+
+    private Transform chatScrollView;
+    private TMPro.TMP_InputField chatInput;
+
+    // Start is called before the first frame update
+    async void Start()
+    {
+        chatInput = GetComponentInChildren<TMPro.TMP_InputField>(true);
+        chatInput.gameObject.SetActive(false);
+
+        chatScrollView = transform.Find("Scroll View/Viewport/Content");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            chatInput.gameObject.SetActive(true);
+            chatInput.GetComponent<TMPro.TMP_InputField>().ActivateInputField();
+            chatOpen = true;
+        }
+
+        // chat interactions below
+        if (!chatOpen) return;
+        // if we press enter, send message
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            SendMessage(chatInput.text);
+            chatInput.gameObject.SetActive(false);
+            chatInput.text = "";
+            chatOpen = false;
+        }
+
+        // if press esc. close chat
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            chatInput.gameObject.SetActive(false);
+            chatInput.text = "";
+            chatOpen = false;
+        }
+    }
+
+    async void SendMessage(string message)
+    {
+        var account = gameManager.burnerManager.CurrentBurner ?? gameManager.masterAccount;
+        // random salt for the message
+        var randomBytes = new byte[28];
+        RandomNumberGenerator.Fill(randomBytes);
+
+        // copy to a 32 byte array
+        var salt = new byte[32];
+        randomBytes.CopyTo(salt, 0);
+
+        salt = salt.Reverse().ToArray();
+
+        var typed_data = TypedData.From(new Message
+        {
+            identity = account.Address,
+            message = message,
+            channel = channel,
+            salt = new FieldElement(salt),
+        });
+
+        Debug.Log(JsonConvert.SerializeObject(typed_data));
+
+        FieldElement messageHash = typed_data.encode(account.Address);
+        Signature signature = account.Signer.Sign(messageHash);
+
+        await worldManager.Publish(typed_data, signature);
+    }
+}
---
+++ Assets/Spawn And Move/GameManager.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using bottlenoselabs.C2CS.Runtime;
+using Dojo;
+using Dojo.Starknet;
+using dojo_bindings;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Object = System.Object;
+using Random = UnityEngine.Random;
+
+// Fix to use Records in Unity ref. https://stackoverflow.com/a/73100830
+using System.ComponentModel;
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal class IsExternalInit{}
+}
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] WorldManager worldManager;
+    [SerializeField] ChatManager chatManager;
+
+    [SerializeField] WorldManagerData dojoConfig;
+    [SerializeField] GameManagerData gameManagerData;
+
+    public BurnerManager burnerManager;
+    private Dictionary<FieldElement, string> spawnedAccounts = new();
+    public Actions actions;
+
+    public JsonRpcClient provider;
+    public Account masterAccount;
+
+
+    void Start()
+    {
+        provider = new JsonRpcClient(dojoConfig.rpcUrl);
+        masterAccount = new Account(provider, new SigningKey(gameManagerData.masterPrivateKey), new FieldElement(gameManagerData.masterAddress));
+        burnerManager = new BurnerManager(provider, masterAccount);
+
+        worldManager.synchronizationMaster.OnEntitySpawned.AddListener(InitEntity);
+        foreach (var entity in worldManager.Entities())
+        {
+            InitEntity(entity);
+        }
+    }
+
+    async void Update()
+    {
+        // dont register inputs if our chat is open
+        if (chatManager.chatOpen) return;
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            spawnedAccounts[masterAccount.Address] = null;
+            var txHash = await actions.spawn(masterAccount);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            var burner = await burnerManager.DeployBurner();
+            spawnedAccounts[burner.Address] = null;
+            var txHash = await actions.spawn(burner);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hit = Physics.Raycast(ray, out var hitInfo);
+            if (hit && hitInfo.transform.parent != null)
+            {
+                var entity = hitInfo.transform.parent;
+                Position position;
+                entity.TryGetComponent(out position);
+
+                if (position && spawnedAccounts.ContainsValue(entity.name))
+                {
+                    var previousBurner = burnerManager.CurrentBurner;
+                    if (previousBurner != null)
+                    {
+                        worldManager.Entity(spawnedAccounts[previousBurner.Address])
+                            .GetComponent<Position>().textTag.color = Color.black;
+                    }
+
+                    var burner = spawnedAccounts.First(b => b.Value == entity.name);
+                    var burnerAddress = burner.Key;
+                    var burnerInstance = burnerManager.Burners.First(b => b.Address == burnerAddress);
+
+                    position.textTag.color = Color.blue;
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // coordinates are different between dojo world and unity
+            Move(new Direction.Down());
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(new Direction.Left());
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(new Direction.Up());
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(new Direction.Right());
+        }
+    }
+
+    private async void Move(Direction direction)
+    {
+        await actions.move(burnerManager.CurrentBurner ?? masterAccount, direction);
+    }
+
+    private void InitEntity(GameObject entity)
+    {
+        // only spawn a capsule if the entity has a position component
+        if (!entity.GetComponent<Position>()) return;
+
+        var capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        // change color of capsule to a random color
+        capsule.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        capsule.transform.parent = entity.transform;
+
+        foreach (var account in spawnedAccounts)
+        {
+            if (account.Value == null)
+            {
+                spawnedAccounts[account.Key] = entity.name;
+                break;
+            }
+        }
+    }
+}
